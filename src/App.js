903ebc3f422348d4125272e9cc7e1e3f88503342
ljs7908 <dev_jisu@naver.com>
2020-04-22 21:57:23 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SinglePage from "./pages/page01";
+import "./App.css";
+
+function App() {
+  return <SinglePage />;
+}
+
+export default App;
---
+++ src/pages/page01.js
@@ -0,0 +1,6 @@
+import React from "react";
+const SinglePage = () => {
+  return <> 첫번째 연습 페이지입니다. </>;
+};
+
+export default SinglePage;
