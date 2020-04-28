ae29ae605fa59df0e877daabc5d0c03d7875c938
ljs7908 <dev_jisu@naver.com>
2020-04-29 02:26:05 +0900

    Helmet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import SinglePage from "./pages/page01";
 import "./App.css";
+import { Helmet } from "react-helmet-async";
 
 function App() {
-  return <SinglePage />;
+  return (
+    <>
+      <Helmet>
+        <title>TEST React App</title>
+      </Helmet>
+      <SinglePage />
+    </>
+  );
 }
 
 export default App;
